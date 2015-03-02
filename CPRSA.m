7c02a517f30128f4b525367e7a293ab3a409909e
tangkk <dengjunqi0632311@gmail.com>
2015-03-02 12:28:31 +0800

    debug

--- CPRSA.m
+++ CPRSA.m
@@ -9,7 +9,7 @@ close all;
 clear;
 clc;
 root = '../AudioSamples/';
-audio = 'tuihou-1.mp3';
+audio = 'heidongli-1.mp3';
 path = [root audio];
 
 % ********************************************************** %
@@ -299,7 +299,7 @@ firsttime = 1;
 oldonset = 1;
 newonset = 1;
 for j = 1:1:sizeS(2)
-    for i = 1:1:min(Sb(j)+6,sizeS(1)) % search upper bounded by Sb + 5
+    for i = 1:1:min(Sb(j)+6,sizeS(1)) % search upper bounded by Sb + 6
         if (So(i,j) > ht && (j - whs > 0 || firsttime == 1)) || j == sizeS(2)
             newonset = i;
             if newonset == oldonset && j < sizeS(2)
@@ -308,6 +308,7 @@ for j = 1:1:sizeS(2)
             if firsttime == 1
                 firsttime = 0;
                 whs = j;
+                oldonset = newonset;
                 break;
             end
             % take the mean over the harmonic window in terms of row
@@ -320,9 +321,7 @@ for j = 1:1:sizeS(2)
             end
             for ii = oldonset:1:sizeS(1) % count from oldonset (elsewise bass inference)
                 gesiiwh = mean(Sg(ii,wh));
-%                 if gesiiwh > 0.10
-                    Sh(ii,wh) = ones(1,length(wh))*gesiiwh;
-%                 end
+                Sh(ii,wh) = ones(1,length(wh))*gesiiwh;
             end
             % normalize the content within harmonic window in terms of col
             for jj = whs:1:whe
@@ -335,7 +334,7 @@ for j = 1:1:sizeS(2)
             end
             % fill the harmonic change vector
             Shv(:,shidx) = Sh(:,whs);
-            Shc(shidx) = whe;
+            Shc(shidx) = whs;
             shidx = shidx + 1;
             whs = j;
             oldonset = newonset;
@@ -346,7 +345,7 @@ end
 nchords = shidx - 1;
 Shc(shidx) = sizeS(2);
 Shv = Shv(:,(1:nchords));
-Shc = Shc(:,(1:nchords));
+Shc = Shc(:,(1:nchords+1)); % boundaries include the endtime
 figure;
 image(k,p,sfactor*Sh);
 set(gca,'YDir','normal');
@@ -416,24 +415,24 @@ set(gca, 'YTick',0:12, 'YTickLabel', bassnotenames);
 % try chord recognition tree method (or chord dictionary method)
 % the chord tree is built this way with priority from top to down
 % --music--            --digital--     --digital difference--
-% 1->3->5 maj          1,5,8            4,7
-% 1->3->5# aug         1,5,9            4,8
-% 1->3->7b dom7        1,5,11           4,10
 % 1->3->7 maj7         1,5,12           4,11
-% 1->3b->5 min         1,4,8            3,7
-% 1->3b->5b dim        1,4,7            3,6
-% 1->3b->6b maj/3      1,4,9            3,8
 % 1->3b->7b min7       1,4,11           3,10
 % 1->3b->7 minmaj7     1,4,12           3,11
-% 1->4->6 maj/5        1,6,10           5,9
+% 1->3->5 maj          1,5,8            4,7
+% 1->3b->5 min         1,4,8            3,7
+% 1->3->5# aug         1,5,9            4,8
+% 1->3b->5b dim        1,4,7            3,6
+% 1->3->7b dom7        1,5,11           4,10
+% 1->3->6 maj6         1,5,10           4,9
+% 1->3b->6 min6        1,4,10           3,9
 % 1->4->5 sus4         1,6,8            5,7
 % 1->2->5 sus2         1,3,8            2,7
+% 1->3b->6b maj/3      1,4,9            3,8
+% 1->4->6 maj/5        1,6,10           5,9
 % 1->2->4 min/7        1,3,6            2,5
 % 1->2->4# maj/7       1,3,7            2,6
 % 1->1#->4 maj/7+      1,2,6            1,5
 % 1->2->7b maj/2       1,3,11           2,10
-% 1->3->6 maj6         1,5,10           4,9
-% 1->3b->6 min6        1,4,10           3,9
 % 1->3b min            1,4              3
 % 1->3 maj             1,5              4
 % 1->5 5               1,8              7
@@ -452,60 +451,87 @@ set(gca, 'YTick',0:12, 'YTickLabel', bassnotenames);
 display('backend-A -- chordtree');
 nchordtype = 28;
 chordtree = cell(2,nchordtype);
-chordtree{1,1} = [4,7];
-chordtree{2,1} = 'maj';
-chordtree{1,2} = [4,8];
-chordtree{2,2} = 'aug';
-chordtree{1,3} = [4,10];
-chordtree{2,3} = 'dom7';
-chordtree{1,4} = [4,11];
-chordtree{2,4} = 'maj7';
+chordtree{1,1} = [4,11];
+chordtree{2,1} = 'maj7';
+
+chordtree{1,2} = [3,10];
+chordtree{2,2} = 'min7';
+
+chordtree{1,3} = [3,11];
+chordtree{2,3} = 'minmaj7';
+
+chordtree{1,4} = [4,7];
+chordtree{2,4} = 'maj';
+
 chordtree{1,5} = [3,7];
 chordtree{2,5} = 'min';
-chordtree{1,6} = [3,6];
-chordtree{2,6} = 'dim';
-chordtree{1,7} = [3,8];
-chordtree{2,7} = 'maj/3';
-chordtree{1,8} = [3,10];
-chordtree{2,8} = 'min7';
-chordtree{1,9} = [3,11];
-chordtree{2,9} = 'minmaj7';
-chordtree{1,10} = [5,9];
-chordtree{2,10} = 'maj/5';
+
+chordtree{1,6} = [4,8];
+chordtree{2,6} = 'aug';
+
+chordtree{1,7} = [3,6];
+chordtree{2,7} = 'dim';
+
+chordtree{1,8} = [4,10];
+chordtree{2,8} = 'dom7';
+
+chordtree{1,9} = [4,9];
+chordtree{2,9} = 'maj6';
+
+chordtree{1,10} = [3,9];
+chordtree{2,10} = 'min6';
+
 chordtree{1,11} = [5,7];
 chordtree{2,11} = 'sus4';
+
 chordtree{1,12} = [2,7];
 chordtree{2,12} = 'sus2';
-chordtree{1,13} = [2,5];
-chordtree{2,13} = 'min/7';
-chordtree{1,14} = [2,6];
-chordtree{2,14} = 'maj/7';
-chordtree{1,15} = [1,5];
-chordtree{2,15} = 'maj/7+';
-chordtree{1,16} = [2,10];
-chordtree{2,16} = 'maj/2';
-chordtree{1,17} = [4,9];
-chordtree{2,17} = 'maj6';
-chordtree{1,18} = [3,9];
-chordtree{2,18} = 'min6';
+
+chordtree{1,13} = [3,8];
+chordtree{2,13} = 'maj/3';
+
+chordtree{1,14} = [5,9];
+chordtree{2,14} = 'maj/5';
+
+chordtree{1,15} = [2,5];
+chordtree{2,15} = 'min/7';
+
+chordtree{1,16} = [2,6];
+chordtree{2,16} = 'maj/7';
+
+chordtree{1,17} = [1,5];
+chordtree{2,17} = 'maj/7+';
+
+chordtree{1,18} = [2,10];
+chordtree{2,18} = 'maj/2';
+
 chordtree{1,19} = 3;
 chordtree{2,19} = 'min';
+
 chordtree{1,20} = 4;
 chordtree{2,20} = 'maj';
+
 chordtree{1,21} = 7;
 chordtree{2,21} = '5';
+
 chordtree{1,22} = 2;
 chordtree{2,22} = '2';
+
 chordtree{1,23} = 5;
 chordtree{2,23} = '4';
+
 chordtree{1,24} = 6;
 chordtree{2,24} = '4#';
+
 chordtree{1,25} = 8;
 chordtree{2,25} = '6b';
+
 chordtree{1,26} = 9;
 chordtree{2,26} = '6';
+
 chordtree{1,27} = 10;
 chordtree{2,27} = '7b';
+
 chordtree{1,28} = 11;
 chordtree{2,28} = '7';
 
@@ -532,22 +558,22 @@ for j = 1:1:sizeShv(2)
             ismatchout = 1;
             % convert slash chords to correct treble name, keep bass num
             upperbass = bass;
-            if i == 7 % maj/3
+            if i == 13 % maj/3
                 upperbass = mod(bass-4-1,12)+1;
             end
-            if i == 10 % maj/5
+            if i == 14 % maj/5
                 upperbass = mod(bass-7-1,12)+1;
             end
-            if i == 13 % min/7
+            if i == 15 % min/7
                 upperbass = mod(bass-10-1,12)+1;
             end
-            if i == 14 % maj/7
+            if i == 16 % maj/7
                 upperbass = mod(bass-10-1,12)+1;
             end
-            if i == 15 % maj/7+
+            if i == 17 % maj/7+
                 upperbass = mod(bass-11-1,12)+1;
             end
-            if i == 16 % maj/2
+            if i == 18 % maj/2
                 upperbass = mod(bass-2-1,12)+1;
             end
             chordogram{1,j} = bass;
@@ -895,60 +921,87 @@ set(gca, 'YTick',0:12, 'YTickLabel', bassnotenames);
 % ****** feedback back end ****** %
 nchordtype = 28;
 chordtree = cell(2,nchordtype);
-chordtree{1,1} = [4,7];
-chordtree{2,1} = 'maj';
-chordtree{1,2} = [4,8];
-chordtree{2,2} = 'aug';
-chordtree{1,3} = [4,10];
-chordtree{2,3} = 'dom7';
-chordtree{1,4} = [4,11];
-chordtree{2,4} = 'maj7';
+chordtree{1,1} = [4,11];
+chordtree{2,1} = 'maj7';
+
+chordtree{1,2} = [3,10];
+chordtree{2,2} = 'min7';
+
+chordtree{1,3} = [3,11];
+chordtree{2,3} = 'minmaj7';
+
+chordtree{1,4} = [4,7];
+chordtree{2,4} = 'maj';
+
 chordtree{1,5} = [3,7];
 chordtree{2,5} = 'min';
-chordtree{1,6} = [3,6];
-chordtree{2,6} = 'dim';
-chordtree{1,7} = [3,8];
-chordtree{2,7} = 'maj/3';
-chordtree{1,8} = [3,10];
-chordtree{2,8} = 'min7';
-chordtree{1,9} = [3,11];
-chordtree{2,9} = 'minmaj7';
-chordtree{1,10} = [5,9];
-chordtree{2,10} = 'maj/5';
+
+chordtree{1,6} = [4,8];
+chordtree{2,6} = 'aug';
+
+chordtree{1,7} = [3,6];
+chordtree{2,7} = 'dim';
+
+chordtree{1,8} = [4,10];
+chordtree{2,8} = 'dom7';
+
+chordtree{1,9} = [4,9];
+chordtree{2,9} = 'maj6';
+
+chordtree{1,10} = [3,9];
+chordtree{2,10} = 'min6';
+
 chordtree{1,11} = [5,7];
 chordtree{2,11} = 'sus4';
+
 chordtree{1,12} = [2,7];
 chordtree{2,12} = 'sus2';
-chordtree{1,13} = [2,5];
-chordtree{2,13} = 'min/7';
-chordtree{1,14} = [2,6];
-chordtree{2,14} = 'maj/7';
-chordtree{1,15} = [1,5];
-chordtree{2,15} = 'maj/7+';
-chordtree{1,16} = [2,10];
-chordtree{2,16} = 'maj/2';
-chordtree{1,17} = [4,9];
-chordtree{2,17} = 'maj6';
-chordtree{1,18} = [3,9];
-chordtree{2,18} = 'min6';
+
+chordtree{1,13} = [3,8];
+chordtree{2,13} = 'maj/3';
+
+chordtree{1,14} = [5,9];
+chordtree{2,14} = 'maj/5';
+
+chordtree{1,15} = [2,5];
+chordtree{2,15} = 'min/7';
+
+chordtree{1,16} = [2,6];
+chordtree{2,16} = 'maj/7';
+
+chordtree{1,17} = [1,5];
+chordtree{2,17} = 'maj/7+';
+
+chordtree{1,18} = [2,10];
+chordtree{2,18} = 'maj/2';
+
 chordtree{1,19} = 3;
 chordtree{2,19} = 'min';
+
 chordtree{1,20} = 4;
 chordtree{2,20} = 'maj';
+
 chordtree{1,21} = 7;
 chordtree{2,21} = '5';
+
 chordtree{1,22} = 2;
 chordtree{2,22} = '2';
+
 chordtree{1,23} = 5;
 chordtree{2,23} = '4';
+
 chordtree{1,24} = 6;
 chordtree{2,24} = '4#';
+
 chordtree{1,25} = 8;
 chordtree{2,25} = '6b';
+
 chordtree{1,26} = 9;
 chordtree{2,26} = '6';
+
 chordtree{1,27} = 10;
 chordtree{2,27} = '7b';
+
 chordtree{1,28} = 11;
 chordtree{2,28} = '7';
 
@@ -972,22 +1025,22 @@ for j = 1:1:lenOut
             ismatchout = 1;
             % convert slash chords to correct treble name, keep bass num
             upperbass = bass;
-            if i == 7 % maj/3
+            if i == 13 % maj/3
                 upperbass = mod(bass-4-1,12)+1;
             end
-            if i == 10 % maj/5
+            if i == 14 % maj/5
                 upperbass = mod(bass-7-1,12)+1;
             end
-            if i == 13 % min/7
+            if i == 15 % min/7
                 upperbass = mod(bass-10-1,12)+1;
             end
-            if i == 14 % maj/7
+            if i == 16 % maj/7
                 upperbass = mod(bass-10-1,12)+1;
             end
-            if i == 15 % maj/7+
+            if i == 17 % maj/7+
                 upperbass = mod(bass-11-1,12)+1;
             end
-            if i == 16 % maj/2
+            if i == 18 % maj/2
                 upperbass = mod(bass-2-1,12)+1;
             end
             newchordogram{1,j} = bass;
